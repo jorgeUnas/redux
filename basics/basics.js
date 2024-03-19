3754becabefcdd49b87751db89763ffb74bc8ea8
EC2 Default User <ec2-user@ip-172-31-38-199.eu-north-1.compute.internal>
2024-03-19 14:56:38 +0000

    Defining a reducer dor the playlist app

--- basics/basics.js
+++ basics/basics.js
@@ -1,3 +1,18 @@
+// Define reducer here
+const reducer = (state = initialState, action) => {
+  switch (action.type){
+    case 'songs/addSong': {
+      return [ ...state, action.payload];
+    }
+    case 'songs/removeSong': {
+      return state.filter(song => song !== action.payload);
+    }
+    default: {
+      return state;
+    }
+  };
+};
+
 const initialState = [ 'Take Five', 'Claire de Lune', 'Respect' ];
 
 const addNewSong = {
